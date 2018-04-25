5b0e9a11a0823f79ac4db5ee69c13e4d41433462
kkkkeeee <zhaikeke1@gmail.com>
2018-04-24 23:54:26 -0400

    added the print number of support vector

--- group12_code/RVC.py
+++ group12_code/RVC.py
@@ -2,9 +2,19 @@
 from rvm import RVC
 from sklearn.multiclass import OneVsOneClassifier
 
-def TrainMyClassifierRVM(X_train, y_train, **kwargs):
+def TrainMyClassifierRVM(X_train, y_train, ifprint = False, **kwargs):
+    #classifier = OneVsOneClassifier(RVC(n_iter=1, **kwargs))
+    #classifier1 = RVC(n_iter=1, verbose=True, **kwargs)
+    #classifier1.fit(X_train, y_train)
     classifier = OneVsOneClassifier(RVC(n_iter=1, **kwargs))
     classifier.fit(X_train, y_train)
+    if ifprint:
+        #print "the number of support vectors is: ", len(classifier.estimators_)
+        numVct = 0
+        for clss in classifier.estimators_:
+            numVct += clss.relevance_.shape[0]
+            print clss.relevance_.shape[0]
+        print "the number of support vectors is: ", numVct
     return classifier
 
 def TestMyClassifierRVM(XTest, EstParameters):
