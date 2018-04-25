ae7c9f53bb3d74824ecfb8a406c59dac8ef8880b
kotaprabhakar <kotaprabhakar@gmail.com>
2018-04-25 15:14:47 -0400

    Added GPR to TrainMyClassifier

--- group12_code/TrainMyClassifier.py
+++ group12_code/TrainMyClassifier.py
@@ -1,31 +1,28 @@
 from sklearn.preprocessing import scale
 from SVM import TrainMyClassifierSVM
 from RVC import TrainMyClassifierRVM
-import GPy
+from GPR import TrainMyClassifierGPR
 
 
-def TrainMyClassifier(XEstimate,XValidate,Parameters,ClassLabels):
+def TrainMyClassifier(XEstimate, XValidate, Parameters, ClassLabels):
     '''
         This function trains the corresponding classifier based on the choice
         The Parameters argument must be a string in the set {'RVM', 'SVM', 'GPR'},
         indicating which classification algorithm to train.
     '''
-    #standardize the estimate and validate data
+    # standardize the estimate and validate data
     XEstimateScaled = scale(XEstimate)
     XValidateScaled = scale(XValidate)
 
     if(Parameters == 'RVM'):
-        EstParameters = TrainMyClassifierRVM(XEstimateScaled,ClassLabels)
+        EstParameters = TrainMyClassifierRVM(XEstimateScaled, ClassLabels)
     elif(Parameters == 'SVM'):
-        EstParameters = TrainMyClassifierSVM(XEstimateScaled,ClassLabels)
+        EstParameters = TrainMyClassifierSVM(XEstimateScaled, ClassLabels)
     elif(Parameters == 'GPR'):
-        EstParameters = TrainMyClassifierGPR(XEstimateScaled,ClassLabels)
+        EstParameters = TrainMyClassifierGPR(XEstimateScaled, ClassLabels)
     else:
-        raise ValueError('Wrong parameters value. The values can either be RVM, SVM or GPR')
+        raise ValueError(
+            'Wrong parameters value. The values can either be RVM, SVM or GPR')
 
     YValidate = EstParameters.predict(XValidateScaled)
     return YValidate, EstParameters
-    
-
-
-    
